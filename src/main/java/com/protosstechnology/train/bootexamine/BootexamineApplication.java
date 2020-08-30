4be633ad4ffbc5e22dc1c8aa9b5184eae9737258
Protoss Technology <protosstechnology@gmail.com>
2020-08-30 11:05:31 +0700

    initial project

---
+++ src/main/java/com/protosstechnology/train/bootexamine/BootexamineApplication.java
@@ -0,0 +1,13 @@
+package com.protosstechnology.train.bootexamine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootexamineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootexamineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protosstechnology/train/bootexamine/BootexamineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protosstechnology.train.bootexamine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootexamineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
